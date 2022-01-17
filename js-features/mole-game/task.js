40386ce8c29cb9e12043297aabe1d5297ba4e537
Elena-Fingali <81174107+Elena-Fingali@users.noreply.github.com>
2022-01-17 12:40:33 +0500

    ready -2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,5 +1,5 @@
-let dead = document.getElementById("dead");
-let lost = document.getElementById("lost");
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
 
 getHole = (index) => {
     let currentHole = document.getElementById(`hole${index}`);
@@ -8,14 +8,18 @@ getHole = (index) => {
             dead.textContent++;
             if(dead.textContent >= 10){
                 alert("Победа");
+            dead.textContent = 0;
+            lost.textContent = 0;
             }
         } else {
             lost.textContent++;
             if (lost.textContent >= 5) {
                 alert("Вы проиграли!");
+             dead.textContent = 0;
+             lost.textContent = 0;   
             }
         }
-    }
+    } 
 };
 
 for (let index = 1; index <=9; index++){
