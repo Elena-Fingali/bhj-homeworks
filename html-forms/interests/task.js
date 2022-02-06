9978c2e58d21fbee331b6ccfd2f8bb0442a7acf8
Elena-Fingali <81174107+Elena-Fingali@users.noreply.github.com>
2022-02-06 14:16:18 +0500

    ready

---
+++ html-forms/interests/task.js
@@ -0,0 +1,22 @@
+let interestMain = document.querySelectorAll('.interests');
+
+for(let item of interestMain) {
+    const closest = item.closest('.interest');
+    if(closest) {
+        const checkBox = closest.querySelector('.interest__check');
+        checkBox.onclick = () => {
+            if(checkBox.checked){
+                invert(closest, true);
+            } else {
+                invert(closest, false);
+            };
+        };
+    };
+};
+
+function invert(element, checked) {
+    let items = element.querySelectorAll('.interest__check');
+    for (let i of items){
+        i.checked = checked;
+    };
+};
